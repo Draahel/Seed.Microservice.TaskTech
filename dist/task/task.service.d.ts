6a7aea3492dfb9e06f03ec3d2aa896374340acb5
Daniel Mercado <dms9812@hotmail.com>
2021-09-03 16:37:12 -0500

    Se injectó el modelo BD para servicios de tareas

--- dist/task/task.service.d.ts
+++ dist/task/task.service.d.ts
@@ -1,5 +1,9 @@
+import { Model } from 'mongoose';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskDocument } from './schemas/task.schema';
 export declare class TaskService {
+    private taskModel;
+    constructor(taskModel: Model<TaskDocument>);
     getTask(id: any): string;
     getTasks(): string;
     createTask(task: CreateTaskDto): string;
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskDocument } from './schemas/task.schema';
 
 @Injectable()
 export class TaskService {
-    
+    constructor(@InjectModel('Task') private taskModel:Model<TaskDocument>){}
     // Obtener una tarea por id
     getTask(id){
         return 'Obtener una tarea';
