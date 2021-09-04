17a596c8adfa7eb938feee693fa5ed9839e39270
Daniel Mercado <dms9812@hotmail.com>
2021-09-04 09:43:40 -0500

    Creacion del archivo create-user.dtp.ts

---
+++ src/user/create-user.dto.ts
@@ -0,0 +1,11 @@
+export class CreateUserDto{
+    name:String;
+    tipoIdent:String;
+    nIdent:Number;
+    email:String;
+    age:Number;
+    cellphone:Number;
+    password:String;
+    state:Boolean;
+    createdAt:Date;
+}
