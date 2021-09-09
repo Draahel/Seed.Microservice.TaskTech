354fdaeef7d45270e80471d7cf6e84b7c3b96db4
Daniel Mercado <dms9812@hotmail.com>
2021-09-09 11:43:03 -0500

    Agregando validaciones a las propiedades de tareas

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,12 +1,29 @@
+import { IsNotEmpty, IsString, MinLength} from "class-validator";
+
 export class CreateTaskDto{
+    
+    @IsString()
+    @IsNotEmpty({message:"La tarea debe tener un titulo."})
     readonly name:String;
+
+    @IsNotEmpty({message:"Debe asignarle un equipo a esta tarea."})
     readonly equipment:String;
+
     readonly assignedTo:String[];
+
+    @IsNotEmpty({message:"Debe tener una descripcion."})
+    @MinLength(15,{message:"La descripcion debe tener minimo 15 caracteres."})
     readonly description:String;
+
     readonly timeStart:String;
+
     readonly timeFinish:String;
+
     readonly dedicatedTime:String;
+
     readonly observatio:String;
+
     readonly state:String;
+
     readonly createdAt:String;
 }
