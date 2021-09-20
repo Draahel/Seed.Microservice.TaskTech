a50cacedf73ecfa8dddc7516ffdc5c0098bdd75e
Daniel Mercado <dms9812@hotmail.com>
2021-09-20 18:34:19 -0500

    instalacion de jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,14 +4,16 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    ) {}
 
   async validateUser(email: string, pass: string){
     const user = await this.userService.findByEmail(email);
     const isMatch = await bcrypt.compare(pass, user.password);
     if (user && isMatch) {
       const { password, ...result } = user;
-      return user;
+      return result;
     }
     return null;
   }
