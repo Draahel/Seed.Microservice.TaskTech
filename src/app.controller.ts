4ff4a85eb946dcdd9d3008c57cd55b6c5e4f8cc9
Daniel Mercado <dms9812@hotmail.com>
2021-09-25 20:23:44 -0500

    Se cambio el Request por el Body para Auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
+import { AuthUserDto } from './auth/dto/AuthUser.dto';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { LocalStrategy } from './auth/local.strategy';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private localStrategy: LocalStrategy,
+  ) {}
 
+  // @UseGuards(LocalAuthGuard)
+  // @Post('auth/login')
+  // async login(@Request() req) {
+  //   return await this.authService.login(req.user);
+  // }
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
-    return await this.authService.login(req.user);
+  async login(@Body() authUserDto: AuthUserDto) {
+    const { email, password } = authUserDto;
+    const user = await this.localStrategy.validate(email, password)
+    // console.log(user);
+    return await this.authService.login(user);
   }
   @UseGuards(JwtAuthGuard)
   @Get('profile')
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -5,12 +5,15 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
   }
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Usuario o contraseña incorrecta');
     }
     return user;
   }
